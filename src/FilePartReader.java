58d170f798732abfdb0cd9c3921f0d1d3331f929
Jani <janimarton22@gmail.com>
2018-11-19 20:29:15 +0100

    FilePartReader Skeleton code done

---
+++ src/FilePartReader.java
@@ -0,0 +1,33 @@
+public class FilePartReader {
+
+    public FilePartReader() {
+    }
+
+    public void setup(String filePath, Integer fromLine, Integer toLine){
+
+        // TODO: 2018.11.19. if: toLine is smaller than fromLine throw: IllegalArgumentException
+        // TODO: 2018.11.19. if fromLine is smaller than 1 throw: IllegalArgumentException
+
+    }
+
+    public String read(){
+        // TODO: 2018.11.19. opens the file on filePath , and gives back it's content as a String
+
+        return "Something String to return";
+    }
+
+    public String readLines(){
+
+        /*
+        TODO: 2018.11.19. reads the file with read ()
+        TODO: 2018.11.19. it gives back every line from it's content between fromLine and toLine
+        (both of them are included), and returns these lines as a String.
+        Take care because if fromLine is 1, it means the very first row in the file.
+        Also, if fromLine is 1 and toLine is 1 also, we will read only the very first line.
+        */
+
+        return "Something String to return";
+    }
+
+
+}
